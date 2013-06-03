d582538ff8cae367023862b63998c36cb167f017
sivareddykothuru <siva@agiliq.com>
2013-06-03 19:44:00 +0530

    task 3 completed

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts import render
+
+from polls.models import Poll
+from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
