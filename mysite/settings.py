8c704d2505ed30cf2163cc13d5b196ba8bbe4fee
sivareddykothuru <siva@agiliq.com>
2013-06-12 16:25:46 +0530

    changed the position of local_settings to make pollapp run in local system

--- mysite/settings.py
+++ mysite/settings.py
@@ -174,10 +174,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
+
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -185,3 +182,8 @@ DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
