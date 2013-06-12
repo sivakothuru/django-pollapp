67c409c607a52d48132f76a0bf6cfd114ecc806a
sivareddykothuru <siva@agiliq.com>
2013-06-12 12:43:29 +0530

    added test cases to the authdetails app

--- authdetails/tests.py
+++ authdetails/tests.py
@@ -6,11 +6,27 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.test import Client
+from polls.models import Poll, Choice
+from django.contrib.auth.models import User
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.user = User.objects.create_user(username="foo",
+                                             email="foo@example.com",
+                                             password="bar")
+        self.c = Client()
+
+    def test_login(self):
+        response = self.c.get("/")
+        self.assertEqual(200, response.status_code)
+        self.c.login(email="foo@example.com", password="bar")
+        response = self.c.get("/")
+        self.assertEqual(200, response.status_code)
+
+    def test_create_user(self):
+        response = self.c.get("/usercreation/")
+        self.assertEqual(200, response.status_code)
+        self.c.login(email="foo@example.com", password="bar", password1="bar")
+        response = self.c.get("/usercreation/")
+        self.assertEqual(200, response.status_code)
