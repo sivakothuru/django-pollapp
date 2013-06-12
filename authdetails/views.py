5142e8f0508621c12a019e909a704b27b734aa4b
sivareddykothuru <siva@agiliq.com>
2013-06-12 16:55:40 +0530

    added HttpResponseRedirect to the views of authdetails app

--- authdetails/views.py
+++ authdetails/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from polls.models import Poll
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout, login, authenticate
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
