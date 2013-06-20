b40539b44c3a4aa92590b91f219d3e50d772f715
sivareddykothuru <siva@agiliq.com>
2013-06-20 15:43:27 +0530

    removed the authenticate fuction from the login view

--- authdetails/views.py
+++ authdetails/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from polls.models import Poll
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth import logout, login
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 
@@ -11,10 +11,10 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
         f = AuthenticationForm(None, request.POST)
         if f.is_valid():
-            login(request, user)
+            username = f.get_user()
+            login(request, username)
             latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
             return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list}, context_instance=RequestContext(request))
     return render_to_response('polls/login.html',{'form': f}, context_instance=RequestContext(request))
